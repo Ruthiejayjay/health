56bd3b07f0681c5068b8ed0a3f573955d9dcfa14
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-02-10 14:48:53 +0100

    added components and images

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="relative h-32 w-32 ...">
+      <div
+        class="
+          fixed
+          bottom-0
+          right-0
+          h-16
+          w-36
+          bg-gradient-to-br
+          from-purple-500
+          via-red-500
+          to-pink-600
+          px-3
+          py-4
+          font-medium
+          text-white
+          rounded-lg
+        "
+      >
+        Contact Us
+      </div>
+    </div>
+    <h-header />
+    <features-1 />
+  </div>
 </template>
 
 <script>
+import Features1 from "~/components/features1.vue";
+import hHeader from "~/components/hHeader.vue";
 export default {
-  name: 'IndexPage'
-}
+  components: { hHeader, Features1 },
+  name: "IndexPage",
+};
 </script>
